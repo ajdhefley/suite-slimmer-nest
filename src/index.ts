eaf974d3bedf1812e3e2cbad006c51d7795c99e7
Ashton Hefley <ajdhefley@gmail.com>
2022-07-28 07:05:41 -0500

    slim -> test for files

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from './nestjs-e2e-test-strategy';
-export * from './nestjs-e2e-slim-suite';
+export * from './nestjs-e2e-test-suite';
 export * from './nestjs-test-strategy';
-export * from './nestjs-slim-suite';
+export * from './nestjs-test-suite';
