13f33d812c032be6d723b56f7df262e8cb366201
Ashton Hefley <ajdhefley@gmail.com>
2022-09-12 19:13:55 -0500

    feat: Use latest slim-suite version, pass in explicit Jest implementation

--- src/nestjs-e2e-test-suite.ts
+++ src/nestjs-e2e-test-suite.ts
@@ -1,10 +1,11 @@
 import { INestApplication } from '@nestjs/common';
 import { TestMockMapper, TestSuite } from 'suite-slimmer';
+import { JestMocker } from './jest-mocker';
 import { e2eNestJSTestStrategy } from './nestjs-e2e-test-strategy';
 
 export class e2eNestJSTestSuite extends TestSuite<INestApplication> {
     constructor(name: string, excludeOthers: boolean = false) {
-        super(name, excludeOthers);
+        super(new JestMocker(), name, excludeOthers);
     }
 
     protected override async initializeTest(mockMapper: TestMockMapper, declarations: any[], imports: any[], providers: any[]) {
@@ -12,7 +13,11 @@ export class e2eNestJSTestSuite extends TestSuite<INestApplication> {
         return strategy.initialize(mockMapper, declarations, imports, providers);
     }
 
-    protected override async initializeTests(mockMapper: TestMockMapper, declarations: any[], imports: any[], providers: any[]): Promise<void> {
+    protected override async initializeTests(mockMapper: TestMockMapper, declarations: any[], imports: any[], providers: any[]) {
         
+    }
+
+    protected override async disposeTests(mockMapper: TestMockMapper, declarations: any[], imports: any[], providers: any[]) {
+
     }
 }
--- src/nestjs-test-suite.ts
+++ src/nestjs-test-suite.ts
@@ -1,11 +1,12 @@
 import { TestMockMapper, TestSuite } from 'suite-slimmer';
+import { JestMocker } from './jest-mocker';
 import { NestJSTestStrategy } from './nestjs-test-strategy';
 
 type Class<T> = new (...args: any[]) => T;
 
 export class NestJSTestSuite<T> extends TestSuite<T> {
     constructor(readonly classType: Class<T>, excludeOthers: boolean = false) {
-        super(classType.name, excludeOthers);
+        super(new JestMocker(), classType.name, excludeOthers);
     }
 
     protected override async initializeTest(mockMapper: TestMockMapper, declarations: any[], imports: any[], providers: any[]) {
@@ -15,5 +16,9 @@ export class NestJSTestSuite<T> extends TestSuite<T> {
 
     protected override async initializeTests(mockMapper: TestMockMapper, declarations: any[], imports: any[], providers: any[]): Promise<void> {
         
+    }
+
+    protected override async disposeTests(mockMapper: TestMockMapper, declarations: any[], imports: any[], providers: any[]) {
+
     }
 }
